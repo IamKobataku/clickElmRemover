0d248e58ae8000c53ed11d7e10243c5861824b3f
kobataku <tk_musik@yahoo.co.jp>
2019-07-07 11:24:08 +0900

    include tabid at domcollection

--- background.js
+++ background.js
@@ -1,61 +1,127 @@
 // インストール時にフラグを初期化する
-chrome.runtime.onInstalled.addListener(function () {
-    chrome.storage.local.set({ flag: false });
-    modeToNormal();
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.local.set({ flag: false });
+  // 削除するDOMの履歴用オブジェクトをローカルストレージに保管する
+  chrome.storage.local.set({ domcollection: [] }); // domcollection: {[tabid:number]: {idcls:string, dispBack:string}[]}
+  modeToNormal();
 });
 // ボタンクリックで切り替え
 chrome.browserAction.onClicked.addListener(toggleAndAction);
 
 // コマンドリスナ
-chrome.commands.onCommand.addListener(function (command) {
-    switch (command) {
-        case "toggle_switch":
-            toggleAndAction();
-            break;
-        case "redo_all":
-            sendMessage({ type: "redo_all" });
-        default:
-            console.log(`Unknown command: ${command}`);
-    }
+chrome.commands.onCommand.addListener(function(command) {
+  switch (command) {
+    case "toggle_switch":
+      toggleAndAction();
+      break;
+    case "remove":
+      break;
+    case "redo":
+      break;
+    case "redo_all":
+      allRedo();
+      break;
+    default:
+      console.log(`Unknown command: ${command}`);
+  }
 });
 
 function toggleAndAction() {
-    // 今のフラグを取得して反転させて
-    chrome.storage.local.get(['flag'], function (result) {
-        const updateFlag = !result.flag
-        chrome.storage.local.set({ flag: updateFlag }, function () {
-            if (updateFlag) {
-                modeToRemove();
-            } else {
-                modeToNormal();
-            }
-        });
+  // 今のフラグを取得して反転させて
+  chrome.storage.local.get(["flag"], function(result) {
+    const updateFlag = !result.flag;
+    chrome.storage.local.set({ flag: updateFlag }, function() {
+      if (updateFlag) {
+        modeToRemove();
+      } else {
+        modeToNormal();
+      }
     });
+  });
 }
 
 /** 削除モードへ */
 function modeToRemove() {
-    chrome.browserAction.setBadgeText({ text: "ON" });
-    chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 100] });
-    sendMessage({ type: "toggle_switch", flag: true })
+  chrome.browserAction.setBadgeText({ text: "ON" });
+  chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 100] });
+  sendMessage({ type: "toggle_switch", flag: true });
 }
 
 /** 通常モードへ */
 function modeToNormal() {
-    chrome.browserAction.setBadgeText({ text: "OFF" });
-    chrome.browserAction.setBadgeBackgroundColor({ color: [128, 128, 128, 100] });
-    sendMessage({ type: "toggle_switch", flag: false });
+  chrome.browserAction.setBadgeText({ text: "OFF" });
+  chrome.browserAction.setBadgeBackgroundColor({ color: [128, 128, 128, 100] });
+  sendMessage({ type: "toggle_switch", flag: false });
+}
+
+function allRedo() {
+  //   sendMessage({ type: "redo_all" });
+  chrome.tabs.query({ active: true }, function(tab) {
+    const tabid = tab[0].id;
+    chrome.storage.local.get(["domcollection"], function(dc) {
+      const col = dc.domcollection[tabid];
+      for (let o = col.pop(); o; o = col.pop()) {
+        redoElm(o);
+      }
+      chrome.storage.local.set({ domcollection: [] });
+    });
+  });
+}
+
+/** エレメントを戻すのをcontent scriptの方に依頼する */
+function redoElm(o) {
+  o.type = "redo";
+  sendMessage(o);
+}
+
+// content scriptからのイベントを記録する
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  switch (request.type) {
+    case "remove":
+      memoryRemoveHist(request, sender.tab.id);
+      break;
+    case "getRedo":
+      takeRedoObj(sender.tab.id, sendResponse);
+      break;
+    default:
+      console.log(`Unknown type: ${request.type}`);
+  }
+  return true;
+});
+
+/** 削除情報を記録 */
+function memoryRemoveHist(o, tabid) {
+  chrome.storage.local.get(["domcollection"], function(dc) {
+    let col = dc.domcollection[tabid];
+    if (!col) {
+      col = [];
+    }
+    col.push({ idcls: o.idcls, dispBack: o.dispBack });
+    dc.domcollection[tabid] = col;
+    chrome.storage.local.set({ domcollection: dc.domcollection });
+  });
+}
+
+/** 最後にpushされたdom情報を返却する */
+function takeRedoObj(tabid, sendResponse) {
+  chrome.storage.local.get(["domcollection"], function(dc) {
+    const col = dc.domcollection[tabid];
+    let result = null;
+    if (col) {
+      result = col.pop();
+    }
+    sendResponse(result);
+  });
 }
 
 /** メッセージ送信 */
 function sendMessage(attr) {
-    chrome.tabs.query({ active: true }, function (tab) {
-        chrome.tabs.sendMessage(tab[0].id, attr);
-    })
+  chrome.tabs.query({ active: true }, function(tab) {
+    chrome.tabs.sendMessage(tab[0].id, attr);
+  });
 }
-
 
 /** 通ってるか確認するためのメソッド。最後は消す */
 function debugMethod(str) {
-    window.alert(str);
+  window.alert(str);
 }
--- content.js
+++ content.js
@@ -1,107 +1,101 @@
-// 削除するDOMの履歴用オブジェクトをローカルストレージに保管する
-chrome.storage.local.set({ domcollection: [] });
-// domcollection: {idcls:string, dispBack:string}[]
 chrome.storage.local.set({ userSelect: document.body.style.userSelect });
 
 // リスナ
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        // console.log(sender.tab ?
-        //     "from a content script:" + sender.tab.url :
-        //     "from the extension");
-        switch (request.type) {
-            case "toggle_switch":
-                if (request.flag) {
-                    turnOn();
-                } else {
-                    turnOff();
-                }
-                break;
-            case "redo_all":
-                allredo();
-                break;
-            default:
-                console.log(`Unknown type: ${request.type}`);
-        }
-    });
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  // console.log(sender.tab ?
+  //     "from a content script:" + sender.tab.url :
+  //     "from the extension");
+  switch (request.type) {
+    case "toggle_switch":
+      if (request.flag) {
+        turnOn();
+      } else {
+        turnOff();
+      }
+      break;
+    case "redo":
+      redoElm(request);
+      break;
+    case "redo_all":
+      allredo();
+      break;
+    default:
+      console.log(`Unknown type: ${request.type}`);
+  }
+});
 
 /** 機能ON */
 function turnOn() {
-    addEvent(window);
-    const iframes = document.getElementsByTagName("iframe");
-    for (let i = 0, l = iframes.length; i < l; i++) {
-        addEvent(iframes[i].contentWindow);
-    }
-    document.body.style.userSelect = "none";
+  addEvent(window);
+  const iframes = document.getElementsByTagName("iframe");
+  for (let i = 0, l = iframes.length; i < l; i++) {
+    addEvent(iframes[i].contentWindow);
+  }
+  document.body.style.userSelect = "none";
 }
 
 /** 機能OFF */
 function turnOff() {
-    removeEvent(window);
-    const iframes = document.getElementsByTagName("iframe");
-    for (let i = 0, l = iframes.length; i < l; i++) {
-        removeEvent(iframes[i].contentWindow);
-    }
-    chrome.storage.local.get(['domcollection'], function (result) {
-        document.body.style.userSelect = result.userSelect;
-    });
+  removeEvent(window);
+  const iframes = document.getElementsByTagName("iframe");
+  for (let i = 0, l = iframes.length; i < l; i++) {
+    removeEvent(iframes[i].contentWindow);
+  }
+  chrome.storage.local.get(["userSelect"], function(result) {
+    document.body.style.userSelect = result.userSelect;
+  });
 }
 
 function addEvent(w) {
-    w.addEventListener("click", clickhandler, true);
+  w.addEventListener("click", clickhandler, true);
 }
 
 function removeEvent(w) {
-    w.removeEventListener("click", clickhandler, true);
+  w.removeEventListener("click", clickhandler, true);
 }
 
 /** クリック時のイベントハンドラ */
 function clickhandler(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    chrome.storage.local.get(['domcollection'], function (result) {
-        const col = result.domcollection;
-        if (e.shiftKey) { // redo
-            const obj = col.pop();
-            if (obj) {
-                redoElm(obj);
-            } else {
-                window.alert("no more history")
-            }
-        } else { // remove
-            const myIDClass = getUniqID("idclsForCER");
-            const elm = e.target;
-            elm.classList.add(myIDClass); // このスクリプト内でIDとして利用するクラスを追加する
-            const dispBack = elm.style.display;
-            col.push({ idcls: myIDClass, dispBack: dispBack });
-            elm.style.display = "none";
-        }
-        chrome.storage.local.set({ domcollection: col });
-    })
-    return false;
-}
-
-function redoElm(obj) {
-    const elm = document.getElementsByClassName(obj.idcls)[0]; // ID(class)からElementをひっぱり
-    if (elm) {
-        elm.style.display = obj.dispBack; // displayを戻して
-        elm.classList.remove(obj.idcls); // ID(class)を消す
-    }
-    // ない場合は無視
-}
-
-/** 全戻し */
-function allredo() {
-    chrome.storage.local.get(['domcollection'], function (result) {
-        const col = result.domcollection;
-        for (let o = col.pop(); o; o = col.pop()) {
-            redoElm(o);
-        }
-        chrome.storage.local.set({ domcollection: [] });
+  e.preventDefault();
+  e.stopPropagation();
+  if (e.shiftKey) {
+    // redo
+    chrome.runtime.sendMessage({ type: "getRedo" }, function(response) {
+      if (response) {
+        redoElm(response);
+      } else {
+        window.alert("no more history");
+      }
     });
+  } else {
+    // remove
+    const o = removeElm(e.target);
+    o.type = "remove"; // メッセージのタイプを挿入
+    chrome.runtime.sendMessage(o);
+  }
+  return false;
+}
+
+/** 指定Elementを消す */
+function removeElm(target) {
+  const myIDClass = getUniqID("idclsForCER");
+  target.classList.add(myIDClass); // このスクリプト内でIDとして利用するクラスを追加する
+  const dispBack = target.style.display;
+  target.style.display = "none";
+  return { idcls: myIDClass, dispBack: dispBack };
+}
+
+/** 消していたElementを再表示する */
+function redoElm(obj) {
+  const elm = document.getElementsByClassName(obj.idcls)[0]; // ID(class)からElementをひっぱり
+  if (elm) {
+    elm.style.display = obj.dispBack; // displayを戻して
+    elm.classList.remove(obj.idcls); // ID(class)を消す
+  }
+  // ない場合は無視
 }
 
 /** ランダムな文字列を渡す */
 function getUniqID(pref) {
-    return `${pref}_${Math.floor(Math.random() * 1000000)}_${Date.now()}`
+  return `${pref}_${Math.floor(Math.random() * 1000000)}_${Date.now()}`;
 }
