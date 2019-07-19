0ca45650250fa0f2e0e1928b6e1117cc99d189d0
kobatakuJP <36495393+kobatakuJP@users.noreply.github.com>
2019-07-19 13:50:31 +0900

    #2 namespace with class

--- content.js
+++ content.js
@@ -1,5 +1,3 @@
-window.mouseOverIframeClsName = ""
-
 // リスナ
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   // console.log(sender.tab ?
@@ -8,215 +6,221 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   switch (request.type) {
     case "toggle_switch":
       if (request.flag) {
-        turnOn();
+        ClickElmRemover.turnOn();
       } else {
-        turnOff();
+        ClickElmRemover.turnOff();
       }
       break;
     case "redo":
-      redoElm(request);
-      break;
-    case "redo_all":
-      allredo();
+      ClickElmRemover.redoElm(request);
       break;
     default:
       console.log(`Unknown type: ${request.type}`);
   }
 });
 
-/** 機能ON */
-function turnOn() {
-  addEvent();
-  backupUserSelectAndToNone();
-  document.body.focus(); // iframe削除に備えてフォーカス移動しておく
-}
+/** このアプリケーションの名前空間を目的としたクラス */
+class ClickElmRemover {
+  /** Iframeにイベントを付与済みであるかどうか */
+  static AddedIframeEventFlag = false;
+  /** マウスオーバーされているiframeのクラス名を入れる。iframeに乗ってない場合は""になる */
+  static mouseOverIframeClsName = "";
+  /** @readonly iframeにsandbox属性を付与するが、もともとついてるパターンもあるはずなのでバックアップ用のオリジナルattributeを用意する */
+  static SANDBOX_BACKUP_ATTR_KEY = "SANDBOX_BACKUP_ATTR_KEY";
+  /** user-selectの値をバックアップしておく */
+  static backupUserSelect = undefined;
 
-/** 機能OFF */
-function turnOff() {
-  removeEvent();
-  restoreUserSelect(window);
-}
+  /** 機能ON */
+  static turnOn  () {
+    this.addEvent();
+    this.backupUserSelectAndToNone();
+    document.body.focus(); // iframe削除に備えてフォーカス移動しておく
+  }
 
-/** Iframeにイベントを付与済みであるかどうか */
-let AddedIframeEventFlag = false;
-/** クリックイベントの追加とiframeのイベント追加 */
-function addEvent() {
-  window.addEventListener("click", clickhandler, true);
-  window.addEventListener('blur', blurHandler, true);
-  addIframeSetting();
-}
+  /** 機能OFF */
+  static turnOff  () {
+    this.removeEvent();
+    this.restoreUserSelect();
+  }
 
-/** iframeに対する設定の追加。クラスIDやイベントリスナなど */
-function addIframeSetting() {
-  const iframes = searchIframes(document);
-  const pref = getRandomPref(); // なぜわざわざランダムIDの接頭語をさらにランダムにするのか？⇒対策されて接頭語*のクラス名を除去するようなスクリプトを組まれないように
-  for (let i = 0, l = iframes.length; i < l; i++) {
-    setSandForIframe(iframes[i]);
-    if (!AddedIframeEventFlag){
-      // iframeにhoverイベント付与していないなら付与する。このイベント及びクラスIDは付与しっぱなしになる。
-      const clsid = getUniqID(pref);
-      iframes[i].classList.add(clsid);
-      // このイベント消すのめんどいのでそのままにするつもり
-      iframes[i].addEventListener("mouseover", () => {window.mouseOverIframeClsName = clsid});
-      iframes[i].addEventListener("mouseout", () => {window.mouseOverIframeClsName = ""});
+  /** クリックイベントの追加とiframeのイベント追加 */
+  static addEvent () {
+    window.addEventListener("click", this.clickhandler, true);
+    window.addEventListener('blur', this.blurHandler, true);
+    this.addIframeSetting();
+  }
+
+  /** iframeに対する設定の追加。クラスIDやイベントリスナなど */
+  static addIframeSetting () {
+    const iframes = this.searchIframes(document);
+    const pref = this.getRandomPref(); // なぜわざわざランダムIDの接頭語をさらにランダムにするのか？⇒対策されて接頭語*のクラス名を除去するようなスクリプトを組まれないように
+    for (let i = 0, l = iframes.length; i < l; i++) {
+      this.setSandForIframe(iframes[i]);
+      if (!this.AddedIframeEventFlag){
+        // iframeにhoverイベント付与していないなら付与する。このイベント及びクラスIDは付与しっぱなしになる。
+        const clsid = this.getUniqID(pref);
+        iframes[i].classList.add(clsid);
+        // このイベント消すのめんどいのでそのままにするつもり
+        iframes[i].addEventListener("mouseover", () => {this.mouseOverIframeClsName = clsid});
+        iframes[i].addEventListener("mouseout", () => {this.mouseOverIframeClsName = ""});
+      }
+    }
+    this.AddedIframeEventFlag = true;
+  }
+
+  /** iframeにsandboxを付与する。 */
+  static setSandForIframe (iframe) {
+    const nowAttr = iframe.getAttribute("sandbox");
+    const backAttr = iframe.getAttribute(this.SANDBOX_BACKUP_ATTR_KEY);
+    if (backAttr === null) {
+      // backがないなら初ONなのでバックアップする(nullの場合は"null"と入るので注意)
+      iframe.setAttribute(this.SANDBOX_BACKUP_ATTR_KEY, nowAttr);
+    }
+    iframe.setAttribute("sandbox", "");
+  }
+
+  /** クリックイベントのみ削除(iframeイベントの削除が冗長になる割に見返り少ないため) */
+  static removeEvent() {
+    window.removeEventListener("click", this.clickhandler, true);
+    window.removeEventListener("blur", this.blurHandler, true);
+    this.restoreIframeSetting();
+  }
+
+  /** iframeの設定をリストア */
+  static restoreIframeSetting() {
+    const iframes = this.searchIframes(document);
+    for (let i = 0, l = iframes.length; i < l; i++) {
+      this.restoreSandForIframe(iframes[i]);
     }
   }
-  AddedIframeEventFlag = true;
-}
 
-/** iframeにsandbox属性を付与するが、もともとついてるパターンもあるはずなのでバックアップ用のオリジナルattributeを用意する */
-const SANDBOX_BACKUP_ATTR_KEY = "SANDBOX_BACKUP_ATTR_KEY";
-/** iframeにsandboxを付与する。 */
-function setSandForIframe(iframe) {
-  const nowAttr = iframe.getAttribute("sandbox");
-  const backAttr = iframe.getAttribute(SANDBOX_BACKUP_ATTR_KEY);
-  if (backAttr === null) {
-    // backがないなら初ONなのでバックアップする(nullの場合は"null"と入るので注意)
-    iframe.setAttribute(SANDBOX_BACKUP_ATTR_KEY, nowAttr);
-  }
-  iframe.setAttribute("sandbox", "");
-}
-
-/** クリックイベントのみ削除(iframeイベントの削除が冗長になる割に見返り少ないため) */
-function removeEvent() {
-  window.removeEventListener("click", clickhandler, true);
-  window.removeEventListener("blur", blurHandler, true);
-  restoreIframeSetting();
-}
-
-/** iframeの設定をリストア */
-function restoreIframeSetting() {
-  const iframes = searchIframes(document);
-  for (let i = 0, l = iframes.length; i < l; i++) {
-    restoreSandForIframe(iframes[i]);
-  }
-}
-
-function restoreSandForIframe(iframe) {
-  const backAttr = iframe.getAttribute(SANDBOX_BACKUP_ATTR_KEY);
-  if(backAttr === null) {
-    // まだ一度もsandboxをセットしていないパターン。変更しない。
-  } else if (backAttr === "null") {
-    // 元々sandboxが設定されていなかったパターン
-    iframe.removeAttribute("sandbox");
-  } else {
-    // リストアすべきパターン
-    iframe.setAttribute("sandbox", backAttr);
-  }
-}
-
-/** iframeのコンテナ要素を洗い出す */
-function searchIframes(elm) {
-  /** iframeを格納している要素群 */
-  let result = [];
-  if (elm.tagName === "IFRAME") {
-    return [elm];
-  }
-  const children = elm.children;
-  if (children.length > 0) {
-    for (let i = 0, l = children.length;i<l;i++) {
-      Array.prototype.push.apply(result, searchIframes(children[i]));
+  /** iframeのsanbox属性をリストアする */
+  static restoreSandForIframe (iframe) {
+    const backAttr = iframe.getAttribute(this.SANDBOX_BACKUP_ATTR_KEY);
+    if(backAttr === null) {
+      // まだ一度もsandboxをセットしていないパターン。変更しない。
+    } else if (backAttr === "null") {
+      // 元々sandboxが設定されていなかったパターン
+      iframe.removeAttribute("sandbox");
+    } else {
+      // リストアすべきパターン
+      iframe.setAttribute("sandbox", backAttr);
     }
   }
-  return result;
-}
 
-/** bodyのuserSelectの値をバックアップしておいてnoneに設定 */
-function backupUserSelectAndToNone() {
-  if (window.backupUserSelect === undefined) {
-    window.backupUserSelect = window.document.body.style.userSelect;
-  }
-  window.document.body.style.userSelect = "none";
-}
-
-/** bodyのuserSelectを戻す */
-function restoreUserSelect(w) {
-  if (w.backupUserSelect !== undefined){
-    w.document.body.style.userSelect = w.backupUserSelect;
-  }
-  // バックアップない場合はなにもしない
-}
-
-/** クリック時のイベントハンドラ */
-function clickhandler(e) {
-  e.preventDefault();
-  e.stopPropagation();
-  if (e.shiftKey) {
-    // redo
-    chrome.runtime.sendMessage({ type: "getRedo" }, function(response) {
-      if (response) {
-        redoElm(response);
-      } else {
-        window.alert("no more history");
+  /** iframeのコンテナ要素を洗い出す */
+  static searchIframes (elm) {
+    /** iframeを格納している要素群 */
+    let result = [];
+    if (elm.tagName === "IFRAME") {
+      return [elm];
+    }
+    const children = elm.children;
+    if (children.length > 0) {
+      for (let i = 0, l = children.length;i<l;i++) {
+        Array.prototype.push.apply(result, this.searchIframes(children[i]));
       }
-    });
-  } else {
-    // remove
-    const o = removeElm(e.target);
-    o.type = "remove"; // メッセージのタイプを挿入
-    chrome.runtime.sendMessage(o);
+    }
+    return result;
   }
-  return false;
-}
 
-/** フォーカスアウト時のイベントハンドラ。iframe用。 */
-function blurHandler(e) {
-  e.preventDefault();
-  e.stopPropagation();
-  if (e.shiftKey) {
-    // redo
-    chrome.runtime.sendMessage({ type: "getRedo" }, function(response) {
-      if (response) {
-        redoElm(response);
-      } else {
-        window.alert("no more history");
-      }
-    });
-  } else {
-    // remove
-    // 削除対象がiframeかどうかを判別してターゲットを変える
-    if (window.mouseOverIframeClsName !== "") {
-      const elm = document.getElementsByClassName(window.mouseOverIframeClsName)[0]
-      const o = removeElm(elm);
+  /** bodyのuserSelectの値をバックアップしておいてnoneに設定 */
+  static backupUserSelectAndToNone () {
+    if (this.backupUserSelect === undefined) {
+      this.backupUserSelect = window.document.body.style.userSelect;
+    }
+    window.document.body.style.userSelect = "none";
+  }
+
+  /** bodyのuserSelectを戻す */
+  static restoreUserSelect () {
+    if (this.backupUserSelect !== undefined){
+      window.document.body.style.userSelect = this.backupUserSelect;
+    }
+    // バックアップない場合はなにもしない
+  }
+
+  /** クリック時のイベントハンドラ */
+  static clickhandler(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    if (e.shiftKey) {
+      // redo
+      chrome.runtime.sendMessage({ type: "getRedo" }, function(response) {
+        if (response) {
+          this.redoElm(response);
+        } else {
+          window.alert("no more history");
+        }
+      });
+    } else {
+      // remove
+      const o = this.removeElm(e.target);
       o.type = "remove"; // メッセージのタイプを挿入
       chrome.runtime.sendMessage(o);
     }
+    return false;
   }
-  document.body.focus(); // 最後にbodyにフォーカスを戻す。iframe連続クリック対策。
-}
 
-/** 指定Elementを消す */
-function removeElm(target) {
-  const myIDClass = getUniqID("idclsForCER");
-  target.classList.add(myIDClass); // このスクリプト内でIDとして利用するクラスを追加する
-  const dispBack = target.style.display;
-  target.style.display = "none";
-  return { idcls: myIDClass, dispBack: dispBack };
-}
-
-/** 消していたElementを再表示する */
-function redoElm(obj) {
-  const elm = document.getElementsByClassName(obj.idcls)[0]; // ID(class)からElementをひっぱり
-  if (elm) {
-    elm.style.display = obj.dispBack; // displayを戻して
-    elm.classList.remove(obj.idcls); // ID(class)を消す
+  /** フォーカスアウト時のイベントハンドラ。iframe用。 */
+  static  blurHandler (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    if (e.shiftKey) {
+      // redo
+      chrome.runtime.sendMessage({ type: "getRedo" }, function(response) {
+        if (response) {
+          this.redoElm(response);
+        } else {
+          window.alert("no more history");
+        }
+      });
+    } else {
+      // remove
+      // 削除対象がiframeかどうかを判別してターゲットを変える
+      if (this.mouseOverIframeClsName !== "") {
+        const elm = document.getElementsByClassName(this.mouseOverIframeClsName)[0]
+        const o = this.removeElm(elm);
+        o.type = "remove"; // メッセージのタイプを挿入
+        chrome.runtime.sendMessage(o);
+      }
+    }
+    document.body.focus(); // 最後にbodyにフォーカスを戻す。iframe連続クリック対策。
   }
-  // ない場合は無視
-}
 
-/** ランダムな文字列を渡す */
-function getUniqID(pref) {
-  return `${pref}_${Math.floor(Math.random() * 1000000)}_${Date.now()}`;
-}
-
-/** 4～10文字のランダム文字列を返す */
-function getRandomPref() {
-  const l = 4 + Math.floor(Math.random() * 7);
-  const c = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const cl = c.length;
-  let r = "";
-  for(var i=0; i<l; i++){
-    r += c[Math.floor(Math.random()*cl)];
+  /** 指定Elementを消す */
+  static removeElm (target) {
+    const myIDClass = this.getUniqID("idclsForCER");
+    target.classList.add(myIDClass); // このスクリプト内でIDとして利用するクラスを追加する
+    const dispBack = target.style.display;
+    target.style.display = "none";
+    return { idcls: myIDClass, dispBack: dispBack };
   }
-  return r;
+
+  /** 消していたElementを再表示する */
+  static redoElm (obj) {
+    const elm = document.getElementsByClassName(obj.idcls)[0]; // ID(class)からElementをひっぱり
+    if (elm) {
+      elm.style.display = obj.dispBack; // displayを戻して
+      elm.classList.remove(obj.idcls); // ID(class)を消す
+    }
+    // ない場合は無視
+  }
+
+  /** ランダムな文字列を渡す */
+  static getUniqID (pref) {
+    return `${pref}_${Math.floor(Math.random() * 1000000)}_${Date.now()}`;
+  }
+
+  /** 4～10文字のランダム文字列を返す */
+  static getRandomPref () {
+    const l = 4 + Math.floor(Math.random() * 7);
+    const c = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const cl = c.length;
+    let r = "";
+    for(var i=0; i<l; i++){
+      r += c[Math.floor(Math.random()*cl)];
+    }
+    return r;
+  }
 }
